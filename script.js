de67d081831a7c1c7355273b9f8e5253c9d3e4db
Shrawan1438 <168614799+Shrawan1438@users.noreply.github.com>
2024-10-08 22:35:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const input = document.querySelector(".input")
+const arrow = document.querySelector(".arrow")
+const list = document.querySelectorAll(".info")
+
+
+const map = L.map('map').setView([28.644800, 77.216721], 13);
+const tileLayer = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+const attribution = { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' }
+const firstTile = L.tileLayer(tileLayer, attribution)
+firstTile.addTo(map)
+let marker=null;
+
+function sendRequest() {
+    let inputValue = input.value
+    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_kNbDCZBbDHD6Uyd6YUFTNcHz5tWUL&ipAddress=${inputValue}`)
+        .then((response) => response.json())
+        .then((responseData) => {
+            if (responseData.code == 422) {
+                alert(responseData.messages)
+            } else {
+                list[0].innerText = responseData.ip
+                list[1].innerText = responseData.location.city + " " + responseData.location.region + " " + responseData.location.country
+                list[2].innerText = responseData.location.timezone
+                list[3].innerText = responseData.isp
+            }
+            map.flyTo([responseData.location.lat,responseData.location.lng],13)
+            if(marker!==null){
+                map.removeLayer(marker)
+            }
+            marker=L.marker([responseData.location.lat,responseData.location.lng])
+            marker.addTo(map)
+            marker.bindPopup(responseData.location.city + " " + responseData.location.region + " " + responseData.location.country).openPopup()
+            marker.addEventListener("click",()=>{
+                map.flyTo([responseData.location.lat,responseData.location.lng],13)
+            })
+        })
+
+
+}
+arrow.addEventListener("click", () => {
+    sendRequest()
+})
+document.addEventListener("keypress", (e) => {
+
+    if (e.key == "Enter") {
+        sendRequest()
+    }
+})
+sendRequest()
